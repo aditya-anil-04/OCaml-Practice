d55183af09405195f93f071fd52b84d9cc16cbe7
Aditya Anil <127827576+aditya-anil-04@users.noreply.github.com>
2025-03-20 16:18:32 -0400

    Update last.ml

--- last.ml
+++ last.ml
@@ -2,7 +2,7 @@
 
 let rec last_one lst = 
   match lst with
-  | [] -> failwith "Empty List"
-  | [x] -> x
-  | _ :: t -> last_element t
+  | [] -> None
+  | [x] -> Some x
+  | _ :: t -> last_one t
 ;;
